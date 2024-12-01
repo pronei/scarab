798b766e6f6efa04c924ac77ea8edf5b6c2d9b66
pronei <pranay.mundra@outlook.com>
2024-11-30 22:19:47 -0800

    perceptron baseline implementation!

--- src/bp/bp_perceptron.c
+++ src/bp/bp_perceptron.c
@@ -14,28 +14,19 @@
 // TODO: drive from params
 #define GRID_DIM_1 16
 #define GRID_DIM_2 1024
-#define PERCEP_HIST_LEN 32
 
 // alt-impl -> index into perceptron just with branch PC
 #define USE_ALT_IDX 1
-#define PERCEP_TABLE_SIZE (1 << 12)
+
+// Jimenez & Lin paper values
+#define PERCEP_HIST_LEN 59
+#define PERCEP_TABLE_SIZE (1 << 10)
 
 #define PERCEP_WEIGHT_INIT_VAL 0
 
 #define DEBUG(proc_id, args...) _DEBUG(proc_id, DEBUG_BP, ##args)
 
 static Bp_Perceptron_Data** bp_percep_data_all_cores = NULL;
-
-#define CMP_ADDR_MASK_CUS (((Addr)-1) << 10)
-
-// used to add 3 bits of proc_id to the 10 bit addr
-// TODO: needs GRID_DIM_2 to be 2^13
-// Addr convert_to_cmp_addr_13bit(uns8 proc_id, Addr addr) {
-//   if((addr & CMP_ADDR_MASK_CUS)) {
-//     addr = addr & ~CMP_ADDR_MASK_CUS;
-//   }
-//   return (addr | (((Addr)proc_id) << 10)) & N_BIT_MASK(13);
-// }
 
 void perceptron_timestamp(Op* op) {}
 void perceptron_spec_update(Op* op) {}
@@ -48,25 +39,12 @@ void perceptron_bp_init(void) {
     for (uns c = 0; c < NUM_CORES; c++) {
         bp_percep_data_all_cores[c] = (Bp_Perceptron_Data*) malloc(sizeof(Bp_Perceptron_Data));
         Bp_Perceptron_Data* bp_percep_data = bp_percep_data_all_cores[c];
+
+        bp_percep_data->perceptron_global_hist = 0;
         bp_percep_data->grid = (Perceptron**)malloc(sizeof(Perceptron*) * GRID_DIM_1);
         
         init_hash_table(&bp_percep_data->table, "Percep_hash_table",
                         PERCEP_TABLE_SIZE, sizeof(Perceptron));
-
-        /*
-            Can use lazy init during prediction...
-        */
-        // for (uns i = 0; i < PERCEP_TABLE_SIZE; i++) {
-        //     Flag new_entry;
-        //     Perceptron* p = (Perceptron *) hash_table_access_create(
-        //         &bp_percep_data->table, i, &new_entry);
-        //     if (new_entry) {
-        //         p->weights = (int32*) malloc(sizeof(int32) * (PERCEP_HIST_LEN+1));
-        //         for(uns k = 0; k < PERCEP_HIST_LEN + 1; k++) {
-        //           p->weights[k] = PERCEP_WEIGHT_INIT_VAL;
-        //         }
-        //     }
-        // }
 
         for(uns i = 0; i < GRID_DIM_1; i++) {
           bp_percep_data->grid[i] = (Perceptron*)malloc(sizeof(Perceptron) * GRID_DIM_2);
@@ -88,10 +66,10 @@ void perceptron_bp_init(void) {
 #define PERCEPTRON_DIM_IDX(addr, dim_entries) ((addr) & N_BIT_MASK(LOG2(dim_entries)))
 #define TRUNCATE_TO_INT64(u) ((int64)((u) & 0x7FFFFFFFFFFFFFFF))
 
-Perceptron* get_perceptron(Op* op) {
-    Bp_Perceptron_Data* bp_percep_data = bp_percep_data_all_cores[PROC_IDX(op->proc_id)];
-    Addr  branch_addr = convert_to_cmp_addr(op->proc_id, op->inst_info->addr);
-    uns32 index1      = PERCEPTRON_DIM_IDX(op->oracle_info.pred_global_hist, GRID_DIM_1);
+Perceptron* get_perceptron(Op* op, Bp_Perceptron_Data* bp_percep_data) {
+    Addr  branch_addr = op->oracle_info.pred_addr;
+    uns64 hist        = bp_percep_data->perceptron_global_hist;
+    uns32 index1      = PERCEPTRON_DIM_IDX(hist, GRID_DIM_1);
     uns32 index2      = PERCEPTRON_DIM_IDX(branch_addr, GRID_DIM_2);
     Perceptron* p;
     // change the impl based on USE_ALT_IDX
@@ -114,26 +92,26 @@ Perceptron* get_perceptron(Op* op) {
 uns8 perceptron_predict(Op* op) {
     uns8  output;
     int32 dot_product = 0;
-    Addr  branch_addr = convert_to_cmp_addr(op->proc_id, op->inst_info->addr);
+    Addr  branch_addr = op->oracle_info.pred_addr;
     uns32 index1      = PERCEPTRON_DIM_IDX(op->oracle_info.pred_global_hist, GRID_DIM_1);
     uns32 index2      = PERCEPTRON_DIM_IDX(branch_addr, GRID_DIM_2);
-    uns32 hist        = op->oracle_info.pred_global_hist;
-    uns32 mask;
-    uns   ii;
+    UNUSED(index1);
+    UNUSED(index2);
+    Bp_Perceptron_Data* bp_percep_data = bp_percep_data_all_cores[PROC_IDX(op->proc_id)];
+    uns64 hist        = bp_percep_data->perceptron_global_hist;
     
-    Perceptron* p = get_perceptron(op);
+    Perceptron* p = get_perceptron(op, bp_percep_data);
     int32* w = &(p->weights[0]);
 
     // start from bias
     dot_product = *(w++);
 
     // compute the dot product
-    for (mask = ((uns32)1) << 31, ii = 0; ii < PERCEP_HIST_LEN;
-         ii++, mask >>= 1, w++) {
-        if (!!(hist & mask))
+    for (uns i = 0; i < PERCEP_HIST_LEN; i++, w++) {
+        if (hist & (1ULL << i))
             dot_product += *w;
         else
-            dot_product += -(*w);
+            dot_product -= *w;
     }
 
     output = dot_product > 0 ? 1 : 0;
@@ -149,28 +127,26 @@ uns8 perceptron_predict(Op* op) {
 }
 
 #define PERCEPTRON_THRESH (int32) (1.93 * (PERCEP_HIST_LEN) + 14)
-// used from bp_conf.c, 127 & -128
-#define MAX_WEIGHT ((1 << (8 - 1)) - 1)
-#define MIN_WEIGHT (-(MAX_WEIGHT + 1))
+#define MAX_WEIGHT_PERCEP ((1 << (10 - 1)) - 1)
+#define MIN_WEIGHT_PERCEP (-(MAX_WEIGHT_PERCEP + 1))
 
 void perceptron_update(Op* op) {
-    // TODO: check if we need to update GHR ourselves or whether bp.c does it for us...
-
     if (op->table_info->cf_type != CF_CBR) {
         return;
     }
 
-    Addr  branch_addr = convert_to_cmp_addr(op->proc_id, op->inst_info->addr);
+    Addr  branch_addr = op->oracle_info.pred_addr;
     uns32 index1      = PERCEPTRON_DIM_IDX(op->oracle_info.pred_global_hist, GRID_DIM_1);
     uns32 index2      = PERCEPTRON_DIM_IDX(branch_addr, GRID_DIM_2);
-    uns32 hist         = op->oracle_info.pred_global_hist;
+    UNUSED(index1);
+    UNUSED(index2);
     int32 dot_product  = op->perceptron_dot_output;
-    int8  correct_pred = op->oracle_info.mispred ? -1 : 1;
+    uns8  correct_pred = op->oracle_info.mispred ? 0 : 1;
     uns8  do_train     = 1;
-    uns32 mask;
-    uns   ii;
 
-    Perceptron* p = get_perceptron(op);
+    Bp_Perceptron_Data* bp_percep_data = bp_percep_data_all_cores[PROC_IDX(op->proc_id)];
+    uns64 hist = bp_percep_data->perceptron_global_hist;
+    Perceptron* p = get_perceptron(op, bp_percep_data);
     int32* w = &(p->weights[0]);
 
     // don't train if the branch was correctly predicted and the output
@@ -187,30 +163,40 @@ void perceptron_update(Op* op) {
             (*w)++;
         else
             (*w)--;
-        if (*w > MAX_WEIGHT)
-            *w = MAX_WEIGHT;
-        else if (*w < MIN_WEIGHT)
-            *w = MIN_WEIGHT;
+        if (*w > MAX_WEIGHT_PERCEP)
+            *w = MAX_WEIGHT_PERCEP;
+        else if (*w < MIN_WEIGHT_PERCEP)
+            *w = MIN_WEIGHT_PERCEP;
 
         // increment w_i if x_i positively correlates with the true direction
-        for (mask = ((uns32)1) << 31, ii = 0; ii < PERCEP_HIST_LEN;
-            ii++, mask >>= 1, w++) {
-            
+        for (uns i = 0; i < PERCEP_HIST_LEN; i++, w++) {
             int32 old_w = *w;
             UNUSED(old_w);
-
-            if (!!(hist & mask) == op->oracle_info.dir) {
+            uns8 match = (hist & (1ULL << i)) > 0;
+            if (match == op->oracle_info.dir) {
                 (*w)++;
-                if (*w > MAX_WEIGHT)
-                    *w = MAX_WEIGHT;
+                if (*w > MAX_WEIGHT_PERCEP)
+                    *w = MAX_WEIGHT_PERCEP;
             } else {
                 (*w)--;
-                if (*w < MIN_WEIGHT)
-                    *w = MIN_WEIGHT;
+                if (*w < MIN_WEIGHT_PERCEP)
+                    *w = MIN_WEIGHT_PERCEP;
             }
             DEBUG(op->proc_id,
                   "index1:%d index2:%d hist:%s old_dot_product:%d correct_pred:%d *w[%d]: %d->%d\n",
-                  index1, index2, hexstr64(hist), dot_product, correct_pred, ii, old_w, *w);
+                  index1, index2, hexstr64(hist), dot_product, correct_pred, i, old_w, *w);
         }
+
+        
     }
+
+    bp_percep_data->perceptron_global_hist <<= 1;
+    bp_percep_data->perceptron_global_hist |= op->oracle_info.dir;
+    // TODO: change PERCEP_HIST_LEN to be driven as param, otherwise compiler
+    // complains while creating a mask...
+    // if (PERCEP_HIST_LEN == 64) {
+    // bp_percep_data->perceptron_global_hist &= ~0ULL;
+    // } else {
+        bp_percep_data->perceptron_global_hist &= ((1ULL << PERCEP_HIST_LEN) - 1);
+    // }
 }
--- src/bp/bp_perceptron.h
+++ src/bp/bp_perceptron.h
@@ -5,11 +5,19 @@
 #include "libs/hash_lib.h"
 #include "globals/global_types.h"
 
+// TODO: required for hist[N:] -> branchPC -> perceptron (or) hist[N:] -> hist xor branchPC -> perceptron approaches
+// typedef struct Perceptrons_Hash_Table_struct {
+//     Hash_Table table;
+// } Perceptrons_Hash_Table;
+
 typedef struct Bp_Perceptron_Data_struct {
     Perceptron** grid;
+    // Perceptrons_Hash_Table* history_to_tables;
     Hash_Table table;
     uns64 perceptron_global_hist;
 } Bp_Perceptron_Data;
+
+Perceptron* get_perceptron(Op*, Bp_Perceptron_Data*);
 
 void perceptron_bp_init(void);
 uns8 perceptron_predict(Op*);
